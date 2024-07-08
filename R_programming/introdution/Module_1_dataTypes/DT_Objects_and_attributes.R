1a0c277e2f6867904433a5438aede7c7aec09630
NzuziMolo <nzuziarmindo@gmail.com>
2024-07-08 17:44:27 -0400

    nova pagina

--- R_programming/introdution/Module_1_dataTypes/DT_Objects_and_attributes.R
+++ R_programming/introdution/Module_1_dataTypes/DT_Objects_and_attributes.R
@@ -29,5 +29,21 @@ X
 x <- vector("numeric", length = 10)
 x
 # de por exemplo,um n=unico tipo de objeto
+
 # lista repersentada como um vetor, ela e uma sequencia de objeto. Mas cada elemento
+
+x <- list(1, "a", TRUE, 1 + 4) 
+x
+
+#Matrices
+#Matrices are vectors with a dimension attribute. The dimension attribute is itself an integer vector of
+#length 2 (nrow, ncol)
+m <- matrix(nrow = 2, ncol = 3) 
+m
+
 # desse vetr pode ser diferente, pode ser um objeto de uma classe diferente
+#Matrices (cont’d)
+#Matrices are constructed column-wise, so entries can be thought of starting in the “upper left” corner
+#and running down the columns.
+m <- matrix(1:6, nrow = 2, ncol = 3) 
+m
---
+++ pratical_r.R
@@ -0,0 +1,12 @@
+
+## instalacao de pacotes
+install.packages("swirl")
+
+packageVersion("swirl")
+
+## Carregar a Biblioteca
+library(swirl)
+install_from_swirl("R Programming")
+
+
+swirl()
